0c0a8a110e3075f2b540fa94a206ebf9bb524e24
rahmatsugiarto <sugiartorahmat187@gmail.com>
2023-10-12 16:10:59 +0700

    delete comment at core_test.dart

--- packages/shared_libraries/core/lib/core_test.dart
+++ packages/shared_libraries/core/lib/core_test.dart
@@ -2,18 +2,3 @@ library core;
 
 export "testing/dummy_data/dummy_objects.dart";
 export "testing/json_reader.dart";
-// export "testing/dummy_data/episode.json";
-// export "testing/dummy_data/movie_detail.json";
-// export "testing/dummy_data/movie_now_playing.json";
-// export "testing/dummy_data/movie_popular.json";
-// export "testing/dummy_data/movie_recommendations.json";
-// export "testing/dummy_data/movie_top_rated.json";
-// export "testing/dummy_data/search_spiderman_movie.json";
-// export "testing/dummy_data/search_spiderman_tv_series.json";
-// export "testing/dummy_data/tv_series_detail.json";
-// export "testing/dummy_data/tv_series_now_playing.json";
-// export "testing/dummy_data/tv_series_popular.json";
-// export "testing/dummy_data/tv_series_recommendations.json";
-// export "testing/dummy_data/tv_series_season_detail.json";
-// export "testing/dummy_data/tv_series_season.json";
-// export "testing/dummy_data/tv_series_top_rated.json";
